69ce09951b7747b9f14be520449ae5d3ab28e7b3
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2022-04-16 15:53:20 +0800

    Form validation

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,55 @@
+const Validation = {
+  validateUrl(urlString) {
+    let url;
+    try {
+      url = new URL(urlString);
+    } catch {
+      return false;
+    }
+
+    return url.protocol === 'http:' || url.protocol == 'https:'; 
+  },
+  displayError(errType, e) {
+    let message;
+
+    switch(errType) {
+      case 'no-url':
+        message = "You forgot the link ಠ_ಠ";
+        break;
+      case 'invalid-url':
+        message = "Url is no bueno";
+        break;
+      case 'invalid-len-id':
+        message = "It said 5 characters, 5!!"
+        break;
+      case 'not-alphanumeric-id':
+        message = "Do you even know what letters and numbers mean"
+
+    }
+
+    $('.invalid-msg').text(message).css('color', '#d22d2d')
+    return e.preventDefault();
+  }
+}
+
 $(document).ready(function(){
-  $('#title').text('Hello Ry2uko');
+  $('form').on('submit', (e) => {
+    const url = $('#urlinput').val();
+    const id = $('#idinput').val();
+    const idRegex = new RegExp('^[a-zA-Z0-9]*$');
+
+    let validateUrl = Validation.validateUrl,
+    displayError = Validation.displayError;
+
+    // Validation
+    return url.length < 1 ? displayError('no-url', e)
+    : !validateUrl(url) ? displayError('invalid-url', e)
+    : id.length !== 5 && id.length !== 0 ? displayError('invalid-len-id', e)
+    : !idRegex.test(id) ? displayError('not-alphanumeric-id', e)
+    : "";
+ 
+  });
+  $('input[type="text"]').on('keypress', () => {
+    $('.invalid-msg').text('easter bunny').css('color', '#fff');
+  });
 });
