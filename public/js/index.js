23f608ba083f99bb88c0029748f14dd39fd6fb54
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2022-04-16 12:07:01 +0800

    Initialize
    Set up server and folders

---
+++ public/js/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('#title').text('Hello Ry2uko');
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE_URl, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', err => console.error(err));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
