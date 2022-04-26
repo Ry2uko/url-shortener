f29396c6e0879daa1d200c1dea9bdffeb1d8edc6
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2022-04-26 21:11:37 +0800

    Update functional_tests.js

--- test/functional_tests.js
+++ test/functional_tests.js
@@ -2,7 +2,6 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
-const req = require('express/lib/request');
 
 chai.use(chaiHttp);
 
